61985242124859936a37a57332c0838d45fbd6ee
Chris Castle <chc@sitecore.net>
2024-04-23 07:58:28 -0500

    ch 12

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -34,8 +34,6 @@ export async function createInvoice(formData: FormData) {
   redirect('/dashboard/invoices');
 }
 
-
-
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function updateInvoice(id: string, formData: FormData) {
@@ -56,3 +54,8 @@ export async function updateInvoice(id: string, formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+  }
